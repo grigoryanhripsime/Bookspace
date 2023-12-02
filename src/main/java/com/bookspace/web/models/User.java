6bccc46be16156b265729f706cb02723d60e13b2
grigoryanhripsime <108063456+grigoryanhripsime@users.noreply.github.com>
2023-12-02 23:36:40 +0400

    created Users table

---
+++ src/main/java/com/bookspace/web/models/User.java
@@ -0,0 +1,27 @@
+package com.bookspace.web.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name="users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String nickname;
+    private String email;
+    private String password;
+    @CreationTimestamp
+    private LocalDateTime date;
+}
