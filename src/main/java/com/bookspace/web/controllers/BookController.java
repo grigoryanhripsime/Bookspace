7e0a90764eadff12ab3954782e5e632efa75586c
grigoryanhripsime <108063456+grigoryanhripsime@users.noreply.github.com>
2024-03-17 14:26:48 +0400

    I created Book class in which I collect info about particular book, then print it

--- src/main/java/com/bookspace/web/controllers/BookController.java
+++ src/main/java/com/bookspace/web/controllers/BookController.java
@@ -1,5 +1,6 @@
 package com.bookspace.web.controllers;
 
+import com.bookspace.web.models.Book;
 import com.bookspace.web.services.BookService;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -8,11 +9,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @RequestMapping("/search")
 public class BookController {
 
     private final BookService bookService;
+    private List<Book> books = new ArrayList<>();
 
     @Autowired
     public BookController(BookService bookService) {
@@ -21,107 +26,111 @@ public class BookController {
 
     @GetMapping("/bySubject")
     private void searchBooksBySubject(String subject) {
-        System.out.println("This is search by genre");
+        System.out.println("This is search by subject, and the subject is: " + subject);
         String jsonResponse = bookService.getBooksBySubject(subject);
 
         // Parse the JSON string into a JSON object
         ObjectMapper objectMapper = new ObjectMapper();
         try {
             JsonNode jsonNode = objectMapper.readTree(jsonResponse);
-
-            // Iterate over the elements in the "docs" array
-            if (jsonNode.has("docs") && jsonNode.get("docs").isArray()) {
-                for (JsonNode doc : jsonNode.get("docs")) {
-                    // Check if the element has the key "author_name" with value "Agatha Christie"
-                    if (doc.has("author_name") && doc.get("author_name").isArray()) {
-                        for (JsonNode authorNode : doc.get("author_name")) {
-                            // Check if the element has the keys "title" and "author_name"
-                            if (doc.has("title") && doc.has("author_name") && doc.has("subject")) {
-                                // Iterate over the subjects in the "subject" array
-                                for (JsonNode subjectNode : doc.get("subject")) {
-                                    // Print the title, author, and genre
-                                    System.out.println("title: " + doc.get("title").asText() + ", author: " + authorNode.asText() + ", genre: " + subjectNode.asText());
-                                }
-                                if (doc.has("cover_i")) {
-                                    String coverImageUrl = "https://covers.openlibrary.org/b/id/" + doc.get("cover_i").asText() + "-L.jpg";
-                                    System.out.println("Cover Image URL: " + coverImageUrl);
-                                }
-
-                            }
-                        }
-                    }
-                }
-            }
+            getBooks(jsonNode);
         } catch (Exception e) {
             e.printStackTrace();
             System.out.println("Error occurred during JSON parsing: " + e.getMessage());
         }
         System.out.println("Search was ended");
+        books.clear();
     }
 
     @GetMapping("/byAuthor")
     private void searchBooksByAuthor(String author) {
-        System.out.println("This is search by author");
+        System.out.println("This is search by author, and the author is: " + author);
         String jsonResponse = bookService.getBooksByAuthor(author);
 
         // Parse the JSON string into a JSON object
         ObjectMapper objectMapper = new ObjectMapper();
         try {
             JsonNode jsonNode = objectMapper.readTree(jsonResponse);
-
-            // Iterate over the elements in the "docs" array
-            if (jsonNode.has("docs") && jsonNode.get("docs").isArray()) {
-                for (JsonNode doc : jsonNode.get("docs")) {
-                    if (doc.has("author_name") && doc.get("author_name").isArray()) {
-                        for (JsonNode authorNode : doc.get("author_name")) {
-                            if (authorNode.asText().equals(author)) {
-                                // Check if the element has the keys "title" and "author_name"
-                                if (doc.has("title") && doc.has("author_name")) {
-                                    // Print the title and the author
-                                    System.out.println("title: " + doc.get("title").asText() + ", author: " + authorNode.asText());
-                                }
-                            }
-                        }
-                    }
-                }
-            }
+            getBooks(jsonNode);
         } catch (Exception e) {
             e.printStackTrace();
             System.out.println("Error occurred during JSON parsing: " + e.getMessage());
         }
         System.out.println("Search was ended");
+        books.clear();
     }
 
     @GetMapping("/byTitle")
     private void searchBooksByTitle(String title) {
-        System.out.println("This is search by title");
+        System.out.println("This is search by title, and the title is: " + title);
         String jsonResponse = bookService.getBooksByTitle(title);
 
         // Parse the JSON string into a JSON object
         ObjectMapper objectMapper = new ObjectMapper();
         try {
             JsonNode jsonNode = objectMapper.readTree(jsonResponse);
-
-            // Iterate over the elements in the "docs" array
-            if (jsonNode.has("docs") && jsonNode.get("docs").isArray()) {
-                for (JsonNode doc : jsonNode.get("docs")) {
-                    // Check if the element has the key "author_name" with value "Agatha Christie"
-                    if (doc.has("author_name") && doc.get("author_name").isArray()) {
-
-                        for (JsonNode authorNode : doc.get("author_name")) {
-                                // Check if the element has the keys "title" and "author_name"
-                                if (doc.has("title") && doc.has("author_name")) {
-                                    // Print the title and the author
-                                    System.out.println("title: " + doc.get("title").asText() + ", author: " + authorNode.asText());
-                                }
-                        }
-                    }
-                }
-            }
+            getBooks(jsonNode);
         } catch (Exception e) {
             e.printStackTrace();
             System.out.println("Error occurred during JSON parsing: " + e.getMessage());
         }
         System.out.println("Search was ended");
+        books.clear();
+    }
+
+    private void getBooks(JsonNode jsonNode) {
+        // Check if the "docs" array is present
+        if (!jsonNode.has("docs") || !jsonNode.get("docs").isArray()) {
+            System.out.println("No documents found in the JSON response.");
+            return;
+        }
+
+        // Iterate over the elements in the "docs" array
+        for (JsonNode doc : jsonNode.get("docs")) {
+            Book book = new Book();
+
+            // Set title if available
+            if (doc.has("title")) {
+                book.setTitle(doc.get("title").asText());
+            }
+
+            // Set authors if available
+            if (doc.has("author_name") && doc.get("author_name").isArray()) {
+                for (JsonNode authorNode : doc.get("author_name")) {
+                    book.setAuthors(authorNode.asText());
+                }
+            }
+
+            // Set subjects if available
+            if (doc.has("subject") && doc.get("subject").isArray()) {
+                for (JsonNode subjectNode : doc.get("subject")) {
+                    book.setSubject(subjectNode.asText());
+                }
+            }
+
+            // Set image if available
+            if (doc.has("cover_i")) {
+                String coverImageUrl = "https://covers.openlibrary.org/b/id/" + doc.get("cover_i").asText() + "-L.jpg";
+                book.setImg(coverImageUrl);
+            }
+
+            // Set rating if available
+            if (doc.has("ratings_average")) {
+                book.setRating(doc.get("ratings_average").asLong());
+            }
+
+            // Add the book to the list
+            books.add(book);
+        }
+        printBooks();
+    }
+
+
+    private void printBooks(){
+        for(Book book : books)
+        {
+            System.out.println(book);
+        }
     }
 }
+
