532c0e9dbca81b5633f6e02ce948e85b886317ff
grigoryanhripsime <108063456+grigoryanhripsime@users.noreply.github.com>
2024-03-17 14:27:05 +0400

    I created Book class in which I collect info about particular book, then print it

--- src/main/java/com/bookspace/web/models/Book.java
+++ src/main/java/com/bookspace/web/models/Book.java
@@ -7,23 +7,32 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-@Entity
-@Table(name = "books")
+//@Entity
+//@Table(name = "books")
 public class Book {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    //@Id
+    //@GeneratedValue(strategy = GenerationType.IDENTITY)
+    //private long id;
     private String title;
-    private String author;
+    private List<String> authors = new ArrayList<>();
     private LocalDateTime pub_date;
     private String img;
     private String description;
-    private String genre;
+    private List<String> subject = new ArrayList<>();
     private float rating;
+
+    public void setAuthors(String author) {
+        this.authors.add(author);
+    }
+    public void setSubject(String subject) {
+        this.subject.add(subject);
+    }
 }
 
